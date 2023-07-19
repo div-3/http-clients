0f19701cb4f0f9938c8fa58d3bcd582579568c8b
div-3 <div-3@mail.ru>
2023-07-19 14:46:15 +0800

    1. Merge ветки API_test2 2. Удалил ненужный рекорд.

--- src/test/java/extentions/ListToDoBeforeAndCreated.java
+++
@@ -1,8 +0,0 @@
-package extentions;
-
-import model.ToDoItem;
-
-import java.util.List;
-
-public record ListToDoBeforeAndCreated(List<ToDoItem> listBeforeCreation, List<ToDoItem> listCreatedItems) {
-}
